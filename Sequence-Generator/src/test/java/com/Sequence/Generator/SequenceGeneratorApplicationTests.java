5f0b2331fcaf766b6cdd5730feb518456ff07ce9
Soumyashree Mohan B <84370845+Soumya-Mohan@users.noreply.github.com>
2024-07-09 15:01:00 +0530

    Add files via upload

---
+++ Sequence-Generator/src/test/java/com/Sequence/Generator/SequenceGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sequence.Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequenceGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
